8b0614fad91e9615bbfa43b87c8afed09de18829
Zabdi <zabdibvb19@gmail.com>
2021-06-09 23:09:17 -0500

    Avance

--- App React JS/yingyang-app/src/App.js
+++ App React JS/yingyang-app/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import NavBar from './Components/NavBar';
+import { Container } from '@material-ui/core';
 import './App.css';
+import Slides from './Components/Slides';
+import BannerTop from './Components/BannerTop';
 
 function App() {
+  const config = {
+    systemName: 'YingYang.com'
+  }
   return (
     <div className="App">
+    <Container maxWidth={'lg'}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <NavBar systemName={config.systemName}/>
+        <Slides></Slides>
+        <BannerTop></BannerTop>
       </header>
+      </Container>
     </div>
   );
 }
